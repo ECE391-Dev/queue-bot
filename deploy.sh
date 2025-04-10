6b3c67d32aa9bd6b4d2a309cf76767e891d77d79
henryq2 <142860172+henryq2@users.noreply.github.com>
2025-04-09 20:25:18 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 screen -S queue-bot -X quit
-screen -S queue-bot -d -m bash -c "cd ~/queue-bot; pkill -f python3; git pull; python3 -m venv venv; source venv/bin/activate; pip3 install -r requirements.txt; python3 queue_bot.py | tee output.log"
+screen -S queue-bot -dm
+screen -r queue-bot -X stuff $"cd ~/queue-bot; pkill -f python3; git pull; python3 -m venv venv; source venv/bin/activate; pip3 install -r requirements.txt; python3 queue_bot.py | tee output.log\n"
