cf51d9f5e79179b05f76a6798b23e330589109e4
henryq2 <142860172+henryq2@users.noreply.github.com>
2025-04-09 19:51:36 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,1 @@
-cd ~/queue_bot
-pkill -f python3
-git pull
-python3 -m venv venv
-source venv/bin/activate
-pip3 install -r requirements.txt
-nohup python3 queue_bot.py > output.log &
-exit
+tmux new-session -d "cd ~/queue_bot; pkill -f python3; git pull; python3 -m venv venv; source venv/bin/activate; pip3 install -r requirements.txt; python3 queue_bot.py | tee output.log"
